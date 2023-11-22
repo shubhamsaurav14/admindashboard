fe1b6e3b8326fce9e25f85c82818e3060ae5c94b
shubham <shubhamsaurav14@gmail.com>
2023-11-22 19:22:45 +0530

    first push

---
+++ src/main/java/com/admin/dashboard/admindashboard/AdmindashboardApplication.java
@@ -0,0 +1,13 @@
+package com.admin.dashboard.admindashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmindashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmindashboardApplication.class, args);
+	}
+
+}
